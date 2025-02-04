07126afd1a464e31d527e57a852297ecd4b77039
Jorge Alejandro Tiscareño Ponce <jtiscareno@isssspea.local>
2025-02-04 15:05:11 -0600

    funciones

--- IntroduccionJS/IntroduccionJS/js/11.js
+++ IntroduccionJS/IntroduccionJS/js/11.js
@@ -1,30 +1,19 @@
-const tecnologias = [
-  "HTML",
-  "CSS",
-  "JavaScript",
-  "React.js",
-  "Node.js",
-  "Nest.js",
-  "TypeScript",
-];
+// Funtion declaration
 
-// for(let i = 0; i < tecnologias.length; i++) {
-//     console.log(tecnologias[i] )
-// }
-
-// forEach
-// tecnologias.forEach(function(tech) {
-//     console.log(tech)
-// })
-
-// map
-const arrayMap = tecnologias.map(function (tech) {
-  return tech;
-});
-
-// for ... of
-for (let tech of tecnologias) {
-  console.log(tech);
+console.log("--Funcion sin parametro---");
+function imprimir() {
+  console.log(1 + 1);
 }
+imprimir();
 
-// console.log(arrayMap)
+console.log("--Funcion con parametro---");
+function sumar(numero1, numero2) {
+  console.log(numero1 + numero2);
+}
+sumar(10, 20);
+
+console.log("---Funcion con parametro por default---");
+function sumar_default(numero1 = 0, numero2 = 0) {
+  console.log(numero1 + numero2);
+}
+sumar_default(10);
--- IntroduccionJS/IntroduccionJS/js/12.js
+++ IntroduccionJS/IntroduccionJS/js/12.js
@@ -1,9 +1,8 @@
-// Function Declaration
-
-function sumar(numero1 = 0, numero2 = 0) {
+// Funciones - Function Expression
+// La diferencia es la forma de declarar la funcion ya que
+// esta se declara y se tiene que ejecutar despues, no como la anterior
+const sumar = function sumar_default(numero1 = 0, numero2 = 0) {
   console.log(numero1 + numero2);
-}
+};
 
 sumar(10, 20);
-sumar(100, 131);
-sumar(10);
